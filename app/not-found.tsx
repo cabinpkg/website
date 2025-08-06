43d643941519aec47faaa525e05b343bc5cca09d
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2025-08-05 20:18:38 -0400

    fix: build error

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ export const metadata: Metadata = {
     title: "404: Not Found",
 };
 
-export default function Custom404(): JSX.Element {
+export default function Custom404(): React.ReactElement {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <h1 className="font-bold m-4 text-6xl">404</h1>
